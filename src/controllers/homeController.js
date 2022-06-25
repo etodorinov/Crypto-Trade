d9046c9ed4b53735d2b2ef615e53dc465b3cc54a
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-06-25 18:14:41 +0300

    Added price fetch for home page coins

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
 const router = require("express").Router();
 
+const businessService = require("../services/businessService");
+
 router.get("/", async (req, res) => {
-  res.render("home");
+  const prices = await businessService.coinsPrice();
+
+  const separate = Object.entries(prices);
+  const bitcoin = Number(separate[0][1]).toFixed(3);
+  const ethereum = Number(separate[1][1]).toFixed(3);
+  const dogecoin = Number(separate[2][1]).toFixed(3);
+
+  res.render("home", { bitcoin, ethereum, dogecoin });
 });
 
 module.exports = router;
