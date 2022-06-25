51674003f3d186d7d9a34a81cfbb7abdb900783a
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-06-25 18:17:14 +0300

    Added link for MongoDB

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,6 @@
-exports.DATABASE_URI = "mongodb://localhost:27017/cryptoTrade";
+exports.DATABASE_URI =
+  "mongodb+srv://todorinov:6x584NaV925Jq5kd2yLKtfzg@cluster0.w6ggb.mongodb.net/?retryWrites=true&w=majority";
+// exports.DATABASE_URI = "mongodb://localhost:27017/cryptoTrade";
 exports.PORT = 3000;
 exports.SALT_ROUNDS = 10;
 exports.COOKIE_ONE = "session";
