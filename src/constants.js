6e620c63dc13fa38d54674532e8ca855543148bf
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-06-26 14:02:48 +0300

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
 exports.DATABASE_URI =
   "mongodb+srv://todorinov:6x584NaV925Jq5kd2yLKtfzg@cluster0.w6ggb.mongodb.net/?retryWrites=true&w=majority";
-// exports.DATABASE_URI = "mongodb://localhost:27017/cryptoTrade";
-exports.PORT = 3000;
+exports.PORT = process.env.PORT;
 exports.SALT_ROUNDS = 10;
 exports.COOKIE_ONE = "session";
 exports.SECRET = "zGzF0UGG4Knh";
