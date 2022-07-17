d200d45c8b1c7349214a7e9e199b44ad5caac04a
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-07-17 09:37:30 +0300

    Add files via upload

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URI =
-  "mongodb+srv://todorinov:6x584NaV925Jq5kd2yLKtfzg@cluster0.w6ggb.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://cryptoCoinsUser:TITYPronEXpla@cluster0.w6ggb.mongodb.net/CryptoCoins?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT;
 exports.SALT_ROUNDS = 10;
 exports.COOKIE_ONE = "session";
